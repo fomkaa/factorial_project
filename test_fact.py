07104be3f4a1c2b492262c9586df67980ba1729b
Kirill <44437125+fomkaa@users.noreply.github.com>
2024-08-11 16:54:19 +0300

    Update test_fact.py

--- test_fact.py
+++ test_fact.py
@@ -1,17 +1,24 @@
-from main import factorial, hand_fact
+from main import factorial, hand_fact, get_elements, extract_links
 import pytest
 import random
 
-numbers = [random.randint(0, 5) for i in range(5)]
+numbers1 = [random.randint(0, 170) for i in range(3)]
+
 @pytest.mark.parametrize("a, rez",[
-                                   (numbers[0],hand_fact(numbers[0])),
-                                   (numbers[1],hand_fact(numbers[1])),
-                                   (numbers[2],hand_fact(numbers[2])),
-                                   (numbers[3],hand_fact(numbers[3])),
-                                   (numbers[4],hand_fact(numbers[4]))
+                                   (numbers1[0],hand_fact(numbers1[0])),
+                                   (numbers1[1],hand_fact(numbers1[1])),
+                                   (numbers1[2],hand_fact(numbers1[2])),
+                                   (171,'Infinity'),
+                                   (588,'Infinity'),
+                                   (999,'Infinity')
                                    ])
+
 def test_func (a,rez):
-    assert (factorial(a)) == rez
+    assert factorial(a) == rez
+
+def test_links():
+    a = [('/privacy', 'Privacy'), ('/terms', 'Terms and Conditions')]
+    assert extract_links(get_elements('https://qa-test.emcd.io')) == a
 
 @pytest.mark.parametrize("a, rez",[('a.s4', 'Please enter an integer'),
                                    ('%^3', 'Please enter an integer'),
@@ -22,6 +29,9 @@ def test_func (a,rez):
 def test_integer(a, rez):
     assert (factorial(a)) == rez
 
-@pytest.mark.parametrize("a",[random.randint(-100, -1) for i in range(5)])
+@pytest.mark.parametrize("a",[random.randint(-100, -1) for i in range(2)])
 def test_under_zero(a):
-    assert (factorial(a)) == '500'
+    assert (factorial(a)) == 'it is impossible to calculate the factorial of a negative number. Please enter a positive integer'
+
+#link='https://qa-test.emcd.io'
+
